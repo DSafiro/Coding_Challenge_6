7d0b39f70e6fd76748af27c6aae5ecdbb7560392
Dylan <safirodylan@gmail.com>
2025-02-09 14:40:43 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -4,5 +4,16 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
     console.log(`Total Profit: $${profit}`) // Outputs total profit
     return profit // Returns profit
 }; // Declares function that calculates profit
-calculateProfit(20,30,100) // Inputs values and produces expected output of $1000
-calculateProfit(50,70,200) // Inputs values and produces expected output of $4000
+calculateProfit(20,30,100); // Inputs values and produces expected output of $1000 as shown in test data
+calculateProfit(50,70,200); // Inputs values and produces expected output of $4000 as shown in test data
+
+// Task 2: Sales Tax Consumption
+function calculateSalesTax(amount, taxrate) {
+    let salesTax = amount * taxrate // Finds sales tax by multiplying amount by tax rate
+    console.log(`Sales Tax: $${Math.floor(salesTax)}`) // Outputs sales tax and rounds it
+    return salesTax // Returns sales tax
+}; // Declares a function that calculates sales tax
+calculateSalesTax(100,0.07); // Inputs values and produces expected output of $7 as shown in test data
+calculateSalesTax(500,0.1); // Inputs values and produces expected output of $50 as shown in test data
+
+
